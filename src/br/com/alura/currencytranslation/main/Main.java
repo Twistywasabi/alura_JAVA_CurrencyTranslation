c868e90ef33563bdb37b8215b80bd676b4d0e9c2
TwistyWasabi17 <lucas-hideaki@hotmail.com>
2024-10-02 09:22:35 -0300

    Http Requests and Responses Added

--- src/br/com/alura/currencytranslation/main/Main.java
+++ src/br/com/alura/currencytranslation/main/Main.java
@@ -1,7 +1,23 @@
 package br.com.alura.currencytranslation.main;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://v6.exchangerate-api.com/v6/7d61ff25c057c9d073e854d8/latest/USD"))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+
+
 
     }
 }
