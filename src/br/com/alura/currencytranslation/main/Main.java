abdb1a56fd5dfba68671815139e5b62dfa26ad76
TwistyWasabi17 <lucas-hideaki@hotmail.com>
2024-10-08 09:45:53 -0300

    First version of currencyTranslation

--- src/br/com/alura/currencytranslation/main/Main.java
+++ src/br/com/alura/currencytranslation/main/Main.java
@@ -3,15 +3,13 @@ package br.com.alura.currencytranslation.main;
 import br.com.alura.currencytranslation.models.ExchangeRates;
 import br.com.alura.currencytranslation.models.ExchangeRatesAYRTech;
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonObject;
 
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -27,6 +25,11 @@ public class Main {
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
         String json = response.body();
+
+        if (response.statusCode() == 403) {
+            System.out.println("Erro no sistema na parte da URL de requisição, entre em contato com o administrador do sistema...");
+            return;
+        }
 
         ExchangeRatesAYRTech exchangeRatesListAYRTech = gson.fromJson(json, ExchangeRatesAYRTech.class);
         ExchangeRates exchangeRatesList = new ExchangeRates(exchangeRatesListAYRTech.conversionRates());
@@ -49,34 +52,41 @@ public class Main {
         while (i != 7){
 
             System.out.println(menu);
-            i =userInput.nextInt();
-            switch (i) {
-                case 1:
-                    exchangeRatesList.convertUsdArs(false);
-                    break;
-                case 2:
-                    exchangeRatesList.convertUsdArs(true);
-                    break;
-                case 3:
-                    exchangeRatesList.convertUsdBrl(false);
-                    break;
-                case 4:
-                    exchangeRatesList.convertUsdBrl(true);
-                    break;
-                case 5:
-                    exchangeRatesList.convertUsdCop(false);
-                    break;
-                case 6:
-                    exchangeRatesList.convertUsdCop(true);
-                    break;
-                case 7:
-                    System.out.println("saindo do sistema...");
-                    break;
-                default:
-                    System.out.println("Digite um número válido");
-                    System.out.println("**************************************************");
-                    break;
+            try {
+                i =userInput.nextInt();
+                switch (i) {
+                    case 1:
+                        exchangeRatesList.convertUsdArs(false);
+                        break;
+                    case 2:
+                        exchangeRatesList.convertUsdArs(true);
+                        break;
+                    case 3:
+                        exchangeRatesList.convertUsdBrl(false);
+                        break;
+                    case 4:
+                        exchangeRatesList.convertUsdBrl(true);
+                        break;
+                    case 5:
+                        exchangeRatesList.convertUsdCop(false);
+                        break;
+                    case 6:
+                        exchangeRatesList.convertUsdCop(true);
+                        break;
+                    case 7:
+                        System.out.println("saindo do sistema...");
+                        break;
+                    default:
+                        System.out.println("Digite um número válido");
+                        System.out.println("**************************************************");
+                        break;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Aconteceu um erro: digite somente números inteiros de 1 a 7");
+                System.out.println(e.getMessage());
+                userInput.nextLine();
             }
+
 
         }
 
